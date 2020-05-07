93cc35b80ab373d741fc39efae34d437c902b08a
Aakash <akashsinghk02@gmail.com>
2020-05-07 18:25:31 +0530

    conflict test

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
-console.log("this is test file");
+console.log("this is test file and this line is changed in master");
 console.log("some changes on test3");
+console.log("thid.line is added in master");
